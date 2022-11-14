29b235845c5f136558505a3098eb051082d62f79
FR_Jess <fr_jess@icloud.com>
2022-11-14 15:13:58 +0100

    [add] all files

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+
+import HeaderApp from './components/HeaderApp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderApp
+  },
+
+  data(){
+    return{
+      titolo: 'Ciao Vue Vite'
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <HeaderApp/>
+  <div class="container">
+    <h1>{{titolo}}</h1>
+  </div>
+
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: 'HeaderApp',
+
+  data(){
+    return{
+      titolo: 'Sono un header'
+    }
+  }
+
+}
+</script>
+
+<template>
+  <h2>{{titolo}}</h2>
+
+</template>
+
+
+<style scoped>
+h2 {
+  color: red;
+}
+
+</style>
